7d3b7409ecc91158818e49f9f5ae30d7ebe17ea4
alenana99 <165273013+alenana99@users.noreply.github.com>
2024-07-02 15:27:50 +0200

    Update map_to_ref.sh

--- Source/map_to_ref.sh
+++ Source/map_to_ref.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+BAM=$1
+REF=$2
+OUT=$3
+samtools fastq "$BAM" -T MM,ML |\
+minimap2 -t 14 --secondary=no -ax map-ont -y "$REF" -|\
+samtools view -b -|\
+samtools sort -@10 -o "${OUT}" -
+samtools index "${OUT}"
